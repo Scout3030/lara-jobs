5f188f10e1ebcd62010405f25ce2ebfe8a92b92d
Roberth <rrodriguezj@uni.pe>
2021-01-12 22:58:09 -0500

    start project

---
+++ resources/views/home/banner.blade.php
@@ -0,0 +1,51 @@
+<section class="banner-area banner-area-three ptb-100">
+    <div class="d-table">
+        <div class="d-table-cell">
+            <div class="container">
+                <div class="banner-content">
+                    <h1>Find The Best Job For Future</h1>
+                    <p>Search Over 10,000 Jobs Today!</p>
+                    <form class="search-job">
+                        <div class="row">
+                            <div class="col-lg-5 col-sm-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="job-title" placeholder="Job title, skills, or company">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="job-title-2" placeholder="City, State, or zip">
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-6 offset-sm-3 offset-lg-0">
+                                <button type="submit" class="default-btn">
+                                    <i class="bx bx-search"></i>
+                                    Search Jobs
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="total-job-count">
+                        <div class="row">
+                            <div class="col-lg-4 col-sm-6 col-md-4">
+                                <div class="job-count">
+                                    <h3><span>Live Jobs: </span>9,492</h3>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-md-4">
+                                <div class="job-count">
+                                    <h3><span>Companies: </span>12,5582</h3>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-md-4 offset-sm-3 offset-md-0 offset-lg-0">
+                                <div class="job-count">
+                                    <h3><span>New Jobs: </span>6,436</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
